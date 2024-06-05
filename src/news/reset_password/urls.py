3c5cf80c8bc0692cfe1e9f56640b05302e5a36de
Ostapenko Igor <skeleton1212@yandex.ru>
2024-06-05 14:51:52 +0300

    successful sending email to user

--- src/news/reset_password/urls.py
+++ src/news/reset_password/urls.py
@@ -6,7 +6,7 @@ app_name = 'reset_password'
 
 urlpatterns = [
     path('', views.CustomPasswordResetView.as_view(), name='reset_password'),
-    path('done/', reset_views.PasswordResetDoneView.as_view(), name='reset_password_done'),
+    path('done/', reset_views.PasswordResetDoneView.as_view(template_name='reset_password/reset_password_done.html'), name='reset_password_done'),
     path('<uidb64>/<token>/', reset_views.PasswordResetConfirmView.as_view(), name ='reset_password_confirm'),
     path('complete/', reset_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
