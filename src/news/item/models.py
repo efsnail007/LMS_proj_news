af302eb531766d73bc81b3a74fd782593f9eeeac
Ostapenko Igor <skeleton1212@yandex.ru>
2024-06-15 11:57:08 +0300

    some fixes with fields

--- src/news/item/models.py
+++ src/news/item/models.py
@@ -3,17 +3,16 @@ from django.conf import settings
 
 # Create your models here.
 
+class Tags(models.Model):
+    name = models.CharField(max_length=100)
+
 
 class Item(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField()
-    tags = models.ManyToMany(Tags)
+    tags = models.ManyToManyField(Tags)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-
-class Tags(models.Model):
-    name = models.CharField(max_length=100)
 
 
 class Addition(models.Model):
