0b52c10f9879ea6b332ecb1333239e8eeae2bb19
Ostapenko Igor <skeleton1212@yandex.ru>
2024-06-15 11:49:08 +0300

    added item and tags tables

--- src/news/item/models.py
+++ src/news/item/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Item(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class NewsTags(models.Model):
+    item = models.
