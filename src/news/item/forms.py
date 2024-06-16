705a83393ed419b8f76694b5a4b4330bac546d49
Ostapenko Igor <skeleton1212@yandex.ru>
2024-06-16 13:41:43 +0300

    creation of news is done

--- src/news/item/forms.py
+++ src/news/item/forms.py
@@ -1,10 +1,35 @@
 from django import forms
 from .models import Tags, Item, Addition
 
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+
 class NewsCreationForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ['text', 'tags']
         labels = {'text': 'Текст'}
-        # widgets = {'tags': forms.ModelMultipleChoiceField(queryset=Tags.objects.all())}
     tags = forms.ModelMultipleChoiceField(queryset=Tags.objects.all(), widget=forms.CheckboxSelectMultiple, label='Теги', to_field_name='name')
+    files = MultipleFileField(label='Файлы')
+
+
+# class AdditionCreationForm(forms.ModelForm):
+#     file = MultipleFileField(label='Файл')
+#     class Meta:
+#         model = Addition
+#         fields = ['file']
--- src/news/item/views.py
+++ src/news/item/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from .models import Tags, Item, Addition
 from .forms import NewsCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -9,3 +12,16 @@ class NewsCreationView(CreateView):
     model = Item
     form_class = NewsCreationForm
     template_name = 'item/create.html'
+    success_url = reverse_lazy('registration:main')
+
+    # def get_context_data(self, *kwargs):
+    #     context = super().get_context_data(*kwargs)
+    #     context['addition_form'] = AdditionCreationForm()
+    #     return context
+
+    def form_valid(self, form):
+        files = form.cleaned_data["files"]
+        form.instance.author = get_object_or_404(User, username=self.request.user.username)
+        for file in files:
+            Addition.objects.create(item=form.save(), file=file)
+        return super().form_valid(form)
