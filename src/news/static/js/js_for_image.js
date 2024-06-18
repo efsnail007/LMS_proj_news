1f6edb919749614b0fb579136e6f7fd63a59d80c
Ostapenko Igor <skeleton1212@yandex.ru>
2024-06-18 21:31:57 +0300

    open photo js is strated

--- src/news/static/js/js_for_image.js
+++ src/news/static/js/js_for_image.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    // обработка чекбокса очистки
     const checkbox = document.getElementById('photo-clear_id');
     const button = document.getElementById('id_photo');
     button.addEventListener("change", (e) => addFile(e.target.files[0]));
@@ -9,16 +10,40 @@ window.onload = function() {
           span.classList.toggle("disabled-span");
         });
     }
+
+    // показ загружаемой фотографии
     function addFile(file) {
       let load_photo = document.getElementById('load-photo');
       if (file) {
           var fr = new FileReader();
           fr.addEventListener("load", function () {
             load_photo.src = fr.result;
-            load_photo.classList.add("photo-border-color");
+            load_photo.classList.add("photo-border");
+            load_photo.addEventListener('click', (e) => image_block_func(load_photo));
             load_photo.classList.remove("hidden-elem");
           }, false);
           fr.readAsDataURL(file);
       }
+    }
+
+    // показ окна с фото
+    var image_block = document.getElementById('image-block-id');
+    var image_block_photo = document.getElementById('image-block-photo-id');
+    var body = document.body,
+    html = document.documentElement;
+    var height = Math.max( body.scrollHeight, body.offsetHeight,
+                       html.clientHeight, html.scrollHeight, html.offsetHeight );
+    image_block.style.height = height+"px";
+    var images = document.querySelectorAll('.photo-border');
+    for (let i = 0; i < images.length; i++) {
+        images[i].addEventListener('click', function() {
+            image_block_func(images[i]);
+        });
+    }
+    function image_block_func(image) {
+        image_block.classList.add("image-block");
+        image_block.classList.remove("hidden-elem");
+        image_block_photo.src = image.src;
+        document.body.style.overflow = "hidden";
     }
 }
