63fa72212176b92401a0c8e2c2786948d9618b9e
Ostapenko Igor <skeleton1212@yandex.ru>
2024-06-20 21:55:34 +0300

    js for image and video view is done and also added some changes in style

--- src/news/static/js/js_for_image.js
+++ src/news/static/js/js_for_image.js
@@ -2,7 +2,7 @@ window.onload = function() {
     // обработка чекбокса очистки
     const checkbox = document.getElementById('photo-clear_id');
     const button = document.getElementById('id_photo');
-    button.addEventListener("change", (e) => addFile(e.target.files[0]));
+    button.addEventListener("change", (e) => addFile(e.target.files));
     const span = document.getElementById('id_photo_');
     if (checkbox) {
         checkbox.addEventListener('change', () => {
@@ -11,19 +11,66 @@ window.onload = function() {
         });
     }
 
-    // показ загружаемой фотографии
-    function addFile(file) {
-      let load_photo = document.getElementById('load-photo');
-      if (file) {
-          var fr = new FileReader();
-          fr.addEventListener("load", function () {
-            load_photo.src = fr.result;
-            load_photo.classList.add("photo-border");
-            load_photo.addEventListener('click', (e) => image_block_func(load_photo));
-            load_photo.classList.remove("hidden-elem");
-          }, false);
-          fr.readAsDataURL(file);
-      }
+    // показ загружаемой фотографии и видео
+    function addFile(files) {
+      let loaded_container = document.querySelector(".loaded-container");
+      for (let i = 0; i < files.length; i++) {
+          if (files[i]) {
+              let column = document.createElement('div');
+              column.classList.add("mt-4");
+              let del = document.createElement('button');
+              del.type = "button";
+              del.classList.add("btn");
+              del.classList.add("btn-danger");
+              del.textContent = "Удалить";
+              del.style.float = "right";
+              del.classList.add("mb-2");
+              column.classList.add("col");
+              if (files[i].type.startsWith('video/')) {
+                let video = document.createElement('video');
+                video.controls = true;
+                let url = URL.createObjectURL(files[i]);
+                video.src = url;
+                video.muted = true;
+                document.body.appendChild(video);
+                video.play();
+                video.loop = "loop";
+                video.classList.add("video-border");
+                video.classList.add("img-thumbnail");
+                video.classList.add("img-fluid");
+                column.appendChild(del);
+                column.appendChild(video);
+              } else {
+                  let fr = new FileReader();
+                  fr.addEventListener("load", function () {
+                    let load_photo = document.createElement('img');
+                    load_photo.src = fr.result;
+                    load_photo.classList.add("photo-border");
+                    load_photo.classList.add("img-thumbnail");
+                    load_photo.classList.add("img-fluid");
+                    load_photo.classList.add("rounded");
+                    load_photo.classList.add("d-block");
+                    load_photo.addEventListener('click', (e) => image_block_func(load_photo));
+                    column.appendChild(del);
+                    column.appendChild(load_photo);
+                  }, false);
+                  fr.readAsDataURL(files[i]);
+              }
+              loaded_container.appendChild(column);
+              del.addEventListener("click", () => del_media(files, files[i], column));
+          }
+        }
+    }
+
+    // обработка удаления из добавленных фото и видео
+    function del_media(files, del_file, column) {
+      let input = document.getElementById('id_photo');
+      let dataTransfer = new DataTransfer()
+      for(const file of input.files)
+        if (file != del_file)
+            dataTransfer.items.add(file);
+      input.files = dataTransfer.files;
+      column.remove();
     }
 
     // показ окна с фото
@@ -35,7 +82,6 @@ window.onload = function() {
             image_block_func(images[i]);
         });
     }
-    //image_block.style.top = window.pageYOffset + "px";
     function image_block_func(image) {
         image_block.style.top = window.pageYOffset + "px";
         image_block.classList.add("image-block");
