8017f354afd39cf63526a17d0101414356257185
Ostapenko Igor <skeleton1212@yandex.ru>
2024-06-21 12:33:45 +0300

    added new subscriotion base

--- src/news/user_page/models.py
+++ src/news/user_page/models.py
@@ -8,3 +8,8 @@ class Profile(models.Model):
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d/', blank=True)
     tags = models.ManyToManyField(Tags)
+
+
+class Subscriptions(models.Model):
+    subscriber = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    subscription_profiles = models.ManyToManyField(Profile)
