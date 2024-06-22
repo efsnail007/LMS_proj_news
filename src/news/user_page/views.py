a63917ea8cbbc98f405719818877eaaf8b41d47d
Ostapenko Igor <skeleton1212@yandex.ru>
2024-06-22 17:05:15 +0300

    created ability to subscribe any user and added ajax for this

--- src/news/user_page/views.py
+++ src/news/user_page/views.py
@@ -1,20 +1,47 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import UpdateView
-from .models import Profile
+from .models import Profile, Subscriptions
 from .forms import UserPageEditForm
 from django.urls import reverse_lazy
 from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 # Create your views here.
 
 class UserPageView(View):
     template_name = 'user_page/user_page.html'
 
+    def __get_is_subscribed(self, request):
+        record_subscription = Subscriptions.objects.get(subscriber_id=request.user.id)
+        all_records = list(record_subscription.subscription_profiles.all())
+        if len(all_records) > 0:
+            return [True if str(item) == self.kwargs['username'] else False for item in
+                         all_records][0]
+        return False
+
     def get(self, request, *args, **kwargs):
         record = Profile.objects.get(user_id=User.objects.get(username=self.kwargs['username']).id)
-        return render(request, self.template_name, {'record': record})
+        return render(request, self.template_name, {'record': record, 'is_subscribed': self.__get_is_subscribed(request)})
 
+    def post(self, request, *args, **kwargs):
+        # if 'subscribe-button-name' in request.POST:
+        #if request.POST.get('subscribe', None) == 'subscribe':
+        form_name = request.POST.get('form_name')
+
+        if form_name == 'subscribe-form':
+            record_subscription = Subscriptions.objects.get(subscriber_id=request.user.id)
+            user_id = User.objects.get(username=self.kwargs['username']).id
+            if not self.__get_is_subscribed(request):
+                record_subscription.subscription_profiles.add(Profile.objects.get(user_id=user_id))
+                return JsonResponse({'success': True, 'text': 'Вы подписаны'})
+            else:
+                record_subscription.subscription_profiles.remove(Profile.objects.get(user_id=user_id))
+                return JsonResponse({'success': True, 'text': 'Подписаться'})
+        return redirect('user_page:user-page', username=self.kwargs['username'])
+
+    @csrf_exempt
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect('registration:auth')
