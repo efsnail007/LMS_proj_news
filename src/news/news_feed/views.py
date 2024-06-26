aac03252419194a9bdb60acc8c08e2fffaee824c
Ostapenko Igor <skeleton1212@yandex.ru>
2024-06-26 16:51:27 +0300

    added some fixes and added ability to filter by subscribed

--- src/news/news_feed/views.py
+++ src/news/news_feed/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from item.models import Item, Addition, Tags
-from user_page.models import Profile
+from user_page.models import Profile, Subscriptions
 from django.http import JsonResponse
 import mimetypes
 from datetime import datetime
@@ -32,35 +32,39 @@ class NewsFeedView(View):
             'addition': self.__get_addition(item)}
             for item in items[page * self.__num_of_items:(page + 1) * self.__num_of_items]]
 
+    def __set_items(self, request, tags):
+        subscriptions = None
+        if request.user.is_authenticated:
+            subscriptions = [Profile.objects.get(id=item.id).user.id for item in
+                             Subscriptions.objects.get(subscriber=request.user.id).subscription_profiles.all()]
+        filter_ar = []
+        for tag in tags:
+            if str(tag) in request.session.get('filter', []):
+                filter_ar.append(tag)
+        if len(filter_ar) > 0:
+            if 'Подписки' in request.session.get('filter', []) and request.user.is_authenticated:
+                return Item.objects.filter(author__in=subscriptions, tags__in=filter_ar).order_by(
+                    '-created_at').distinct(), filter_ar.append('Подписки')
+            return Item.objects.filter(tags__in=filter_ar).order_by('-created_at').distinct(), filter_ar
+        else:
+            if 'Подписки' in request.session.get('filter', []) and request.user.is_authenticated:
+                return Item.objects.filter(author__in=subscriptions).order_by('-created_at').distinct(), filter_ar.append('Подписки')
+        return Item.objects.all().order_by('-created_at'), []
+
 
     def get(self, request, *args, **kwargs):
         tags = Tags.objects.all()
-        items = Item.objects.all().order_by('-created_at')
         page = int(request.GET.get('page', 1))
         if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
             action = request.GET.get('action')
             filter = request.GET.get('filter').split()
             request.session['filter'] = filter
-            filter_ar = []
-            for tag in tags:
-                if str(tag) in request.session['filter']:
-                    filter_ar.append(tag)
-            if len(filter_ar) > 0:
-                items = Item.objects.filter(tags__in=filter_ar).order_by('-created_at').distinct()
+            items, _ = self.__set_items(request, tags)
             locale.setlocale(locale.LC_ALL, 'ru_RU.UTF-8')
             if action == 'feed':
                 items_for_unauthenticated = self.__get_items(items, page)
                 return JsonResponse({'all_data': items_for_unauthenticated, 'page': page})
-
-        filter_session = request.session.get('filter', None)
-        context_filter_session = None
-        if filter_session:
-            items = Item.objects.filter(tags__in=[Tags.objects.get(name=filter_session[i]) for i in range(len(filter_session))]).order_by('-created_at').distinct()
-            context_filter_session = filter_session
-        else:
-            context_filter_session = []
-
+        items, context_filter_session = self.__set_items(request, tags)
         items_for_unauthenticated = [[item, Profile.objects.get(user_id=item.author.id), self.__get_addition(item)] for
                                      item in items[:self.__num_of_items]]
-
         return render(request, self.template_name, {'items_for_unauthenticated': items_for_unauthenticated, 'tags': tags, 'filter_session': context_filter_session})
