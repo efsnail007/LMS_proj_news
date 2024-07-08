60f8cf261895f806ac6bbf035cd88d2f71bea7d5
MiTrOsH <nikita.gorbunov.2003@mail.ru>
2024-07-08 12:33:37 +0300

    added errors

--- src/news/item/views.py
+++ src/news/item/views.py
@@ -18,6 +18,18 @@ def get_addition(item):
         type_and_file[str(file)] = [mimetype.split('/')[0], file.id]
     return type_and_file
 
+def handler404(request, exception):
+    return render(request, 'errors/404.html', status=404)
+
+def handler500(request):
+    return render(request, 'errors/500.html', status=500)
+
+def handler503(request):
+    return render(request, 'errors/503.html', status=503)
+
+def handler403(request, exception):
+    return render(request, 'errors/403.html', status=403)
+
 
 class NewsCreationView(CreateView):
     model = Item
